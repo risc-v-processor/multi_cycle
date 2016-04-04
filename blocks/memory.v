d966296e9a599298b412c41e1dfdf60cd58320c3
Nagarathna123 <nagarathna.nayak.1994@gmail.com>
2016-04-04 20:29:35 +0530

    Update memory.v

--- blocks/memory.v
+++ blocks/memory.v
@@ -1,27 +1,27 @@
 `timescale 1ns / 1ps
 
-`define MEM_VECTOR_SIZE 64
-`define I_MEM_SIZE 32
+`define MEM_VECTOR_SIZE 128 // total memory size 128 bytes
+`define I_MEM_SIZE 32 // first 32 bytes for instruction memory 32:126 corresponds to data memory
 `define WORD 2'b10
-`define MEM_MAP_IO_ADDRESS 128
+`define MEM_MAP_IO_ADDRESS 127 // memory mapped io address
 `define BUS_WIDTH 32
 
 module memory(
     input clk,
     input rst,
-	 input wr_en,
+    input wr_en, 
     input [(`BUS_WIDTH-1):0] address,
-	 input [(`BUS_WIDTH-1):0] in_data,
-	 input [1:0] mem_size,
-	 input sz_ex,
-	 output reg [(`BUS_WIDTH-1):0] mem_map_io,
+    input [(`BUS_WIDTH-1):0] in_data,
+    input [1:0] mem_size,
+    input sz_ex,
+    output reg [(`BUS_WIDTH-1):0] mem_map_io,
     output reg [(`BUS_WIDTH-1):0] out_data
     );
 
 reg [1:0] m_size ;
 reg m_sz_ex ;
 reg mem_map_io_wr_en ,m_wr_en ;
-// reg [31:0] m_out_data ,m_mem_map_io ;
+//Instantiating mem module
 mem MEM (
       .clk(clk),
 		.rst(rst),
@@ -35,25 +35,25 @@ mem MEM (
 		
 always@(posedge clk)
   begin
-    if( address < `MEM_VECTOR_SIZE ) begin
-	   if( address < `I_MEM_SIZE ) begin 
-		    m_size <= `WORD ;
-		    m_sz_ex <= 1'bx ;
+    if( address < `MEM_VECTOR_SIZE ) begin // valid address
+	   if( address < `I_MEM_SIZE ) begin  // Instruction memory address range
+		    m_size <= `WORD ; // Default 32 bits
+		    m_sz_ex <= 1'bx ; 
 		end
 		
-		else if(address > `I_MEM_SIZE && address != `MEM_MAP_IO_ADDRESS) begin 
-		     m_size <= mem_size ;
-           m_sz_ex <= sz_ex ;
+		else if(address > `I_MEM_SIZE && address != `MEM_MAP_IO_ADDRESS) begin //Data Memory address range
+		     m_size <= mem_size ; // number of bytes to be loaded depends on the input
+                     m_sz_ex <= sz_ex ; //sign extension signal
             end     
 	     end
    end
  always @(posedge clk) begin 
-	    if(address == `MEM_MAP_IO_ADDRESS ) begin 
-		      if(wr_en == 1'b1 ) begin
+	    if(address == `MEM_MAP_IO_ADDRESS ) begin // address corresonds to memory mapped io
+		      if(wr_en == 1'b1 ) begin //enabling of  write operation to memory mapped address
 				    mem_map_io_wr_en = 1'b1 ;
 					  m_wr_en = 1'b0 ;
 					 end
-				else if(wr_en == 1'b0) begin
+				else if(wr_en == 1'b0) begin // write is disabled and data is read from the memory
 				    mem_map_io_wr_en = 1'b0 ;
 					  m_wr_en = 1'b0 ;
 				    out_data <= mem_map_io ;
@@ -63,10 +63,10 @@ always@(posedge clk)
 
 always@(posedge clk) begin 
   if( rst == 1'b1 ) begin 
-     mem_map_io <= 32'b0 ;
+     mem_map_io <= 32'b0 ; //On reset , set to zero 
      end
 	else if(mem_map_io_wr_en == 1'b1 ) begin
-	      mem_map_io <= in_data ;
+	      mem_map_io <= in_data ; 
      	 end
   end	 
  
